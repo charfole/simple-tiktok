47f4abd7e2615c8988a3ad95352c0f8ba2e04d84
charfole <1335991934@qq.com>
2023-02-17 17:04:24 +0800

    add middleware to follow router

--- router/router.go
+++ router/router.go
@@ -34,9 +34,9 @@ func InitRouter(r *gin.Engine) {
 	apiRouter.GET("/comment/list/", controller.CommentList)
 
 	// extra apis - II
-	apiRouter.POST("/relation/action/", mycontroller.RelationAction)
-	apiRouter.GET("/relation/follow/list/", mycontroller.FollowList)
-	apiRouter.GET("/relation/follower/list/", mycontroller.FollowerList)
+	apiRouter.POST("/relation/action/", middleware.JWTMiddleware(), mycontroller.RelationAction)
+	apiRouter.GET("/relation/follow/list/", middleware.JWTMiddleware(), mycontroller.FollowList)
+	apiRouter.GET("/relation/follower/list/", middleware.JWTMiddleware(), mycontroller.FollowerList)
 	apiRouter.GET("/relation/friend/list/", controller.FriendList)
 	apiRouter.GET("/message/chat/", controller.MessageChat)
 	apiRouter.POST("/message/action/", controller.MessageAction)
